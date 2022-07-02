4b00ce0ac12257694c8b7755748d877ed978e2bc
HuskYzPinto <pokepin1@hotmail.com>
2022-07-02 15:25:28 +1000

    add checkpalindrome

--- cmd/check-palindrome/main.go
+++ cmd/check-palindrome/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func checkPalindrome(word string) bool {
+	reversedString := ""
+	for _, value := range word {
+		reversedString = string(value) + reversedString
+	}
+	if reversedString == word {
+		return true
+	}
+	return false
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input your text: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%t\n", checkPalindrome(strings.TrimSpace(input)))
 }
